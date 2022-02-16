52024b3a18fa84ab954426f6886a5a20e1bcf155
hannahhall <hannahkhall13@gmail.com>
2022-02-16 14:36:12 -0600

    route to login if unauthorized

--- data/fetcher.js
+++ data/fetcher.js
@@ -1,3 +1,20 @@
 const baseUrl = 'http://localhost:8000'
-export const fetchWithResponse = (url, options) => fetch(`${baseUrl}/${url}`, options).then(res => res.json())
+const checkError = (res) => {
+  if (!res.ok) {
+    throw Error(res.statusText);
+  }
+  return res
+}
+
+const catchError = (err) => {
+  console.log(err)
+  window.location.href = "/login"
+}
+
+export const fetchWithResponse = (url, options) => fetch(`${baseUrl}/${url}`, options)
+  .then(checkError)
+  .then(res => res.json())
+  .catch(catchError)
+
 export const fetchWithoutResponse = (url, options) => fetch(`${baseUrl}/${url}`, options)
+  .then(checkError)
